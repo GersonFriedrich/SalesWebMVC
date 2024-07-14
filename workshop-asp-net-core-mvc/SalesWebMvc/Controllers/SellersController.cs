13907c17af0dff15be345648763b59aaac3440b6
Gerson Friedrich <gersonsojogos@gmail.com>
2024-07-14 11:31:23 -0300

    Validation

--- workshop-asp-net-core-mvc/SalesWebMvc/Controllers/SellersController.cs
+++ workshop-asp-net-core-mvc/SalesWebMvc/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using SalesWebMvc.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace SalesWebMvc.Controllers
 {
@@ -22,7 +23,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Index()
         {
-            var list = _sellerService.FindAll();
+            var list = _sellerService.FindAll().OrderBy(x => x.Name);
             return View(list);
         }
 
@@ -38,6 +39,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -105,6 +113,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
